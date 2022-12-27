ad31cfc3a8d585cf9dcfe42738f75214e781b52e
DE BARROS Geoffrey <debarrosg01@gmail.com>
2022-12-27 12:20:43 +0100

    Ajout de localStorage et de petites finition css

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const form = document.querySelector("form");
 
+// Local storage
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un truc pour le supprimer</li>`;
+  }
+}
+
+window.addEventListener("load", getTodos);
+
 // Ajouter un élément a la liste
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   list.innerHTML += `<li>${item.value}</li>`;
   item.value = "";
+  storeList();
 });
 
 //Supprimer un élément de la liste
@@ -14,4 +30,5 @@ list.addEventListener("click", (e) => {
   } else {
     e.target.classList.add("checked");
   }
+  storeList();
 });
